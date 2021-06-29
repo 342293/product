bba1f72cdb283033b76d436857efcc4bebe673b3
342293 <2037491469@qq.com>
2021-06-29 17:52:27 +0800

    17:52

--- my-product/src/pages/Home/Home.js
+++ my-product/src/pages/Home/Home.js
@@ -5,6 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
+    Height:0,
     list:[
       {
         id:1,
@@ -43,12 +44,22 @@ Page({
       }
     ]
   },
-
+  getSystemInfo(){
+    wx.getSystemInfo({
+      success:h => {
+        let custom = wx.getMenuButtonBoundingClientRect();
+        const customButton = custom.bottom + custom.top - h.statusBarHeight;
+        this.setData({
+          Height:customButton
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getSystemInfo()
   },
 
   /**
--- my-server/config/config.js
+++ my-server/config/config.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize('product', 'root', '123456', {
     host: "localhost",
-    dialect: 'mysql'
+    dialect: 'mysql',
+    logging:false,
 })
 
 module.exports = sequelize
