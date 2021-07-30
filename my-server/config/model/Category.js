adc390dc488f175554efa1c4d09a63b1bdbb3f2e
product <2037491469@qq.com>
2021-07-30 09:11:52 +0800

    product

--- my-server/config/model/Category.js
+++ my-server/config/model/Category.js
@@ -27,9 +27,24 @@ const secondary_classification = sequelize.define('secondary_classification', {
     freezeTableName: true
 })
 
+// const three_level_classification = sequelize.define('three_level_classification', {
+//     id: {
+//         type: Sequelize.INTEGER(11),
+//         primaryKey: true,
+//         autoIncrement: true,
+//     },
+//     title:Sequelize.STRING,
+//     three_level_classification_ppid:Sequelize.STRING
+// }, {
+//     timestamps: false,
+//     freezeTableName: true
+// })
+
 category.hasMany(secondary_classification,{ as:'children', foreignKey:"secondary_classification_pid",sourceKey:"id"})
+// secondary_classification.hasMany(three_level_classification,{foreignKey:"three_level_classification_ppid",sourceKey:"secondary_classification_pid"})
 
 module.exports = {
     category,
-    secondary_classification
+    secondary_classification,
+    // three_level_classification
 }
